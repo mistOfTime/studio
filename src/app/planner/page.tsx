2c9890e40b1682c017ee4386250861b7ebdb1220
Sien Jandave Saldaña <sienjandaves@gmail.com>
2026-03-29 11:31:04 +0000

    add 3 hours in duration

--- src/app/planner/page.tsx
+++ src/app/planner/page.tsx
@@ -258,6 +258,7 @@ export default function PlannerPage() {
                   <SelectItem value="60">1 hour</SelectItem>
                   <SelectItem value="90">1.5 hours</SelectItem>
                   <SelectItem value="120">2 hours</SelectItem>
+                  <SelectItem value="180">3 hours</SelectItem>
                 </SelectContent>
               </Select>
             </div>
@@ -333,6 +334,7 @@ export default function PlannerPage() {
                           <SelectItem value="60">1 hour</SelectItem>
                           <SelectItem value="90">1.5 hours</SelectItem>
                           <SelectItem value="120">2 hours</SelectItem>
+                          <SelectItem value="180">3 hours</SelectItem>
                         </SelectContent>
                       </Select>
                     </div>
